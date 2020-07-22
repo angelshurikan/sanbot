f0f59c42f5c9ca4cb65a4f1df39f24f900949310
angelshurikan <angelshurikan@gmail.com>
2020-07-22 16:09:00 +0200

    [+] 1ere version avec script de sanbot

---
+++ app/src/main/java/com/sanbot/librarydemod/MainActivity.java
@@ -0,0 +1,37 @@
+package com.sanbot.librarydemod;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sanbot.librarydemod.video.MediaControlActivity;
+import com.sanbot.librarydemod.video.SoftDecodeActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        startActivity(new Intent(MainActivity.this,MediaControlActivity.class));
+    }
+
+    public void mainStreamHard(View view) {
+        startActivity(new Intent(MainActivity.this,MediaControlActivity.class).putExtra("type",1));
+    }
+
+    public void subStreamHard(View view) {
+        startActivity(new Intent(MainActivity.this,MediaControlActivity.class).putExtra("type",2));
+    }
+
+    public void subStreamNV21(View view) {
+        startActivity(new Intent(MainActivity.this,SoftDecodeActivity.class).putExtra("type",2));
+    }
+
+    public void mainStreamNV21(View view) {
+        startActivity(new Intent(MainActivity.this,SoftDecodeActivity.class).putExtra("type",1));
+    }
+}
